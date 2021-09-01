fbfab510f51dccf432a54d3363d9ddb26bea8dc1
Fuyang Liu <liufuyang@users.noreply.github.com>
2021-09-01 22:51:41 +0200

    test qucc bms

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ set -o pipefail
 set -o xtrace
 
 readonly TARGET_HOST=root@venus.local
-readonly TARGET_PATH=/home/root/usb-1
+readonly TARGET_PATH=/home/root/usb-qucc
 readonly TARGET_ARCH=armv7-unknown-linux-musleabihf
-readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/usb-1
+readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/usb-qucc
 
 cargo build --target=${TARGET_ARCH} --release 
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
--- src/qucc/main.rs
+++ src/qucc/main.rs
@@ -1,24 +1,25 @@
+use crc16::{State, MODBUS};
 use serialport::{available_ports, SerialPort};
 use std::io::{Read, Write};
 use std::time::Duration;
 
 fn main() {
-    println!("Hello, world! Raspberry Pi!");
-
     match available_ports() {
         Ok(it) => println!("{:?}", it),
         Err(e) => println!("{}", e),
     }
 
-    let mut port = serialport::new("/dev/ttyUSB0", 9600)
+    let mut port = serialport::new("/dev/tty.usbserial-110", 9600)
         .timeout(Duration::from_secs(2))
         .open_native()
         .expect("Failed to open port");
 
     println!("timeout: {}", port.timeout().as_secs());
 
-    let decoded = hex::decode("a58090080000000000000000bd").expect("Decoding failed");
-    println!("{:?}", decoded);
+    let mut decoded = hex::decode("010310180003").expect("Decoding failed");
+    let ck = State::<MODBUS>::calculate(decoded.as_slice());
+    decoded.append(&mut ck.to_le_bytes().to_vec());
+    println!("{}", hex::encode(decoded.clone()));
 
     // Write format:
     // head,  tag, cmd,  len,  data                            , check
@@ -27,17 +28,14 @@ fn main() {
     port.write_all(decoded.as_slice());
 
     // Reply format:
-    // head,  tag, cmd,  len,  data                            , check
-    // 0xA5, 0x01, 0x90, 0x08, [b0, b1, b2, b3, b4, b5, b6 ,b7], 0x??
-    // [165,    1,  144,    8,  0, 217, 0,  0, 117, 48, 0, 209,  141]
-    let mut buffer = [0; 13];
+    let mut buffer = [0; 11];
     let i = port.read(&mut buffer).expect("read failed");
     println!("read: {}", i);
-    println!("{:?}", buffer);
+    println!("{}", hex::encode(buffer));
 
-    let soc_result = usb_1::parse_90(buffer).unwrap();
-    println!(
-        "voltage: {}V, current: {}A, soc: {}%",
-        soc_result.0, soc_result.1, soc_result.2
-    );
+    // let soc_result = usb_1::qucc::parse_cell_voltage(buffer).unwrap();
+    // println!(
+    //     "voltage: {}V, current: {}A, soc: {}%",
+    //     soc_result.0, soc_result.1, soc_result.2
+    // );
 }
