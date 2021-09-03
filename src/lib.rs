cebe16c739e8e78e2a858f526db812a3581112db
Fuyang Liu <liufuyang@users.noreply.github.com>
2021-09-03 21:55:43 +0200

    Add a gui

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod daly;
 mod error;
 pub mod qucc;
+pub mod qucc_egui;
---
+++ src/qucc_egui/bms_cache.rs
@@ -0,0 +1,44 @@
+use crate::qucc::QuccBMS;
+use std::error::Error;
+use std::ops::Sub;
+use std::time::{Duration, SystemTime};
+
+const REFRESH_RATE: Duration = Duration::from_secs(5);
+
+pub struct BMS {
+    bms: QuccBMS,
+    last_check_time: SystemTime,
+    cell_v: Vec<u16>,
+}
+
+impl BMS {
+    pub fn new(bms: QuccBMS) -> BMS {
+        BMS {
+            bms,
+            last_check_time: SystemTime::now().sub(Duration::from_secs(60)),
+            cell_v: Vec::new(),
+        }
+    }
+
+    fn ready_to_refresh(&mut self) -> bool {
+        let now = SystemTime::now();
+        if now
+            .duration_since(self.last_check_time)
+            .unwrap()
+            .ge(&REFRESH_RATE)
+        {
+            self.last_check_time = now;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn get_cell_v(&mut self) -> Result<Vec<u16>, Box<dyn Error>> {
+        if self.ready_to_refresh() {
+            let cell_v = self.bms.get_cell_v()?;
+            self.cell_v = cell_v;
+        }
+        Ok(self.cell_v.clone())
+    }
+}
